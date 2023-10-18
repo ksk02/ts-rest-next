1ada218794af5db6cbf441fea72483100595fed7
ksk02 <nahanaha02@gmail.com>
2023-10-18 21:28:52 +0900

    swc導入

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        swcLoader: true,
+        swcMinify: true,
+    },
+}
 
 module.exports = nextConfig
